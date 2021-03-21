34a0e61818910c7b89690a472cebd696fb76cf11
Viz <vishvanthsaravanakumar@Vishvanths-MacBook-Air.local>
2021-03-22 00:21:43 +0530

    testing march 22nd 12:20
    Signed-off-by: vishvanth <vishvanthsaravanakumar@gmail.com>

--- app/src/main/java/com/example/collegetourapp/ActivityAbout.java
+++ app/src/main/java/com/example/collegetourapp/ActivityAbout.java
@@ -1,5 +1,5 @@
 package com.example.collegetourapp;
-
+//test
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- app/src/main/java/com/example/collegetourapp/MainActivity.java
+++ app/src/main/java/com/example/collegetourapp/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.collegetourapp;
-
+//Testing commit march 22nd
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
