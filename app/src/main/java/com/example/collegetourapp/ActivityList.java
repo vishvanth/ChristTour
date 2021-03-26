1980a51ab0fe33bda80b80cddc58de517ea77d51
vishvanth <vishvanthsaravanakumar@gmail.com>
2021-03-27 03:52:23 +0530

    3:50 Am
    Signed-off-by: vishvanth <vishvanthsaravanakumar@gmail.com>

--- app/src/main/java/com/example/collegetourapp/ActivityList.java
+++ app/src/main/java/com/example/collegetourapp/ActivityList.java
@@ -2,13 +2,32 @@ package com.example.collegetourapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityList extends AppCompatActivity {
+    private Button buttonBlock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        buttonBlock = findViewById(R.id.blocks);
+        buttonBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityResult();
+            }
+        });
+    }
+
+    public void openActivityResult() {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
